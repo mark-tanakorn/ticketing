38d8fd960edded9219af315b037dc1281dc8bf2e
Nick <mark.tanakorn@trustedhub.com>
2025-12-15 16:52:07 +0800

    remove tav_opensource

--- tav_opensource/backend/app/core/nodes/builtin/data/__init__.py
+++
@@ -1,8 +0,0 @@
-"""data nodes."""
-
-from .csv_export import CSVExportNode
-from .json_parser import JSONParserNode
-from .pdf_export import PDFExportNode
-from .csv_value_extractor import CSVValueExtractorNode
-
-__all__ = ["CSVExportNode", "JSONParserNode", "PDFExportNode", "CSVValueExtractorNode"]
